b8d07a7f2dc73d4eda82311eb0a0b62f8d9ec972
toth_szilvia <szylwy@gmail.com>
2022-06-29 23:48:47 +0200

    add material ui

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
+import Typography from '@mui/material/Typography';
 
 function Profile() {
   return (
     <>
-        <h1>Your name</h1>
-        <h2>your Passport id</h2>
+        <Typography variant='h1'>Your name</Typography>
+        <Typography variant='h2'>Your passport ID</Typography>
     </>
   )
 }
